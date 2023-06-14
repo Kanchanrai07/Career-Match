9981bb2ef4d4e4577bde99b362d402c63cd0ec1c
Kanchanrai07 <kanchanrai@tuicms.edu.np>
2023-06-14 10:13:58 +0545

    changed

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php
 @include 'config.php';
+session_start();
 if(isset($_POST['submit'])){
     $name = mysqli_real_escape_string($conn,$_POST['name']);
     $email = mysqli_real_escape_string($conn,$_POST['email']);
     $pass = md5($_POST['password']);
     $cpass = md5($_POST['cpassword']);
+    $user_type=$_POST['user_type'];
 
     $select= "SELECT * FROM user_form WHERE email = '$email' && password ='$pass'";
 
@@ -12,7 +14,10 @@ if(isset($_POST['submit'])){
 
     if(mysqli_num_rows($result)>0){
       $row = mysqli_fetch_array($result);
-     
+     if($row['user_type'] == 'admin'){
+        $_SESSION['admin_name'] = $row['name'];
+        header('location:postjob.php');
+     }
     }
 };
 
@@ -40,7 +45,7 @@ if(isset($_POST['submit'])){
                 };
             };
             ?>
-            <input type="email" name="email" required placeholder="Enter your email id">
+            <input type="email" name="email" required placeholder="Enter your email">
             <input type="password" name="password" required placeholder="Enter your password">
             <input type="submit" name="submit" value="Login" class="form-btn" href="shop.php">
             <p>Don't have an account? <a href="signin.php">SignUp</a></p>
