055b0fc91131d6047b996ea7f69d0f9f998b3f78
Kanchanrai07 <kanchanrai@tuicms.edu.np>
2023-06-19 11:49:23 +0545

    done

---
+++ searchjob.php
@@ -0,0 +1,19 @@
+<?php include 'nav.php'
+?>
+<style>
+    .container{
+        width: 100%;
+        display: flex;
+    align-items: center;
+    }
+    .content{
+        display: flex;
+        align-items: center; 
+        padding-top: 0px;
+    }
+    </style>
+<div class="container">
+    <div class="content">
+    <h1>Search fo Job</h1>
+    </div>
+</div>
